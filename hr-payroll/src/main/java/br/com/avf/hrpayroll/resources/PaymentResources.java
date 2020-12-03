a6a844d242349a4e00a4f9db4df163bbeca7b3c9
Angelo Vicente Filho <angelo.vicente@veolia.com>
2020-12-03 13:16:29 -0300

    [hr-eureka-server] Configuraiton Hystrix

--- hr-payroll/src/main/java/br/com/avf/hrpayroll/resources/PaymentResources.java
+++ hr-payroll/src/main/java/br/com/avf/hrpayroll/resources/PaymentResources.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import br.com.avf.hrpayroll.entities.Payment;
 import br.com.avf.hrpayroll.services.PaymentService;
 
@@ -23,10 +25,15 @@ public class PaymentResources {
 		this.service = service;
 	}
 	
+	@HystrixCommand(fallbackMethod = "getPaymentAlternative")
 	@GetMapping(value = "/{workerId}/days/{days}")
 	public ResponseEntity<?> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
+	}
+	
+	public ResponseEntity<?> getPaymentAlternative(Long workerId, Integer days) {
+		throw new RuntimeException("O serviço de pagamento está fora do ar!");
 	}
 	
 }
