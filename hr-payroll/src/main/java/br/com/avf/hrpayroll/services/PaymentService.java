b5e1788efb848ecdb0230db44d3e272945c8ed10
Angelo Vicente Filho <angelo.vicente@veolia.com>
2020-12-03 10:33:08 -0300

    [hr-payroll] payroll

--- hr-payroll/src/main/java/br/com/avf/hrpayroll/services/PaymentService.java
+++ hr-payroll/src/main/java/br/com/avf/hrpayroll/services/PaymentService.java
@@ -1,16 +1,33 @@
 package br.com.avf.hrpayroll.services;
 
-import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import br.com.avf.hrpayroll.entities.Payment;
+import br.com.avf.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
 
-	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	private final RestTemplate template;
+
+	@Autowired
+	public PaymentService(RestTemplate template) {
+		this.template = template;
+	}
+
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Payment Angelo", new BigDecimal(200), days);
+		Map<String, Object> params = new HashMap<>();
+		params.put("id", workerId);
+		Worker worker = template.getForObject(workerHost + "/workers/{id}", Worker.class, params);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
