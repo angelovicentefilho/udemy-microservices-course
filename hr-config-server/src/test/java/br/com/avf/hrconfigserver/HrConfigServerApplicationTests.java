bee84db1f600599bb60c19ce8839ce0426aa07b3
Angelo Vicente Filho <angelo.vicente@veolia.com>
2020-12-03 16:03:25 -0300

    [hr-config-server] Central configuration

---
+++ hr-config-server/src/test/java/br/com/avf/hrconfigserver/HrConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.avf.hrconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
